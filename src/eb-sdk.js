d1d91d469ea658c645cd13336a9fd66fc03c4731
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-15 15:31:20 +0200

    Add Documnation for eb-sdk.js methods

--- src/eb-sdk.js
+++ src/eb-sdk.js
@@ -3,10 +3,19 @@ AWS.config.update({region: 'eu-west-1'});
 const elasticbeanstalk = new AWS.ElasticBeanstalk()
 
 module.exports = {
+
+  /**
+   * @method updateEnvironmentVariables runs the sdk's function updateEnvironment
+   * and updates the environment with the environment variables supploed by
+   * the user in the main program
+   * @param {object} params the paramaters object that contains 
+   * the name of the environment and the environmetn variables to update
+   */
   updateEnvironmentVariables: (params) => {
     elasticbeanstalk.updateEnvironment(params, (err, data) => {
       if (err) console.log(err, err.stack)
       else     console.log(data)
     }) 
   }
+
 }
