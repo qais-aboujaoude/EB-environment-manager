2ded1d76529f30588cc922ba1dc87784ec9b6959
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-21 21:42:16 +0200

    Refactor - file_reader is a private  non exported method
    Also added documentation

--- src/csv-parser.js
+++ src/csv-parser.js
@@ -1,28 +1,34 @@
 const fs = require('fs'),
       readline = require('readline')
 
-const csv_parser = module.exports = {
-
   /**
-   * @method file_reader
+   * @method file_reader opens an interface to reads and parse csv file  
+   * @param file_to_parse the csv file to read
+   * @return {object} a readline object 
    */
-  file_reader: file_to_parse => {
+const file_reader = file_to_parse => {
     return rl = readline.createInterface({
       input: fs.createReadStream(file_to_parse),
       terminal: false
     })
-  },
+  }
+
+const csv_parser = module.exports = {
 
   /**
-   * @method pasre_csv_to_array
+   * @async
+   * @method pasre_csv_to_array reada csv file line by line. splits the line at 
+   * a comma and pushes it into an array
+   * @param file_to_parse the csv file to read and parse
+   * @return {array} parsed_array, an array contains the parsed csv file
    */
   pasre_csv_to_array: file_to_parse => {
     const parsed_array = []
-    csv_parser.file_reader(file_to_parse)
+    file_reader(file_to_parse)
     return new Promise((resolve, reject) => {
       rl.on('line', input => {
         let index = input.indexOf(',')
-        parsed_array.push([input.slice(0, index), input.slice(index+1)])
+        parsed_array.push([input.slice(0, index), input.slice(index + 1)])
       })
       rl.on('close', () => {
         parsed_array.length 
