81ac255dc2acde80c3089fa03c7038d6b3b017f1
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-21 14:17:15 +0200

    Initial Commit on new branch

--- src/eb-sdk.js
+++ src/eb-sdk.js
@@ -8,14 +8,15 @@ module.exports = {
    * @method updateEnvironmentVariables runs the sdk's function updateEnvironment
    * and updates the environment with the environment variables supploed by
    * the user in the main program
-   * @param {object} params the paramaters object that contains 
+   * @param {object} params the paramaters object that contains
    * the name of the environment and the environmetn variables to update
    */
   updateEnvironmentVariables: (params) => {
     elasticbeanstalk.updateEnvironment(params, (err, data) => {
       if (err) console.log(err, err.stack)
       else     console.log(data)
-    }) 
+    })
   }
 
 }
+
