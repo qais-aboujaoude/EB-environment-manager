cd850cee4d1c4fbbe3fba8e02f8ac753db40e92f
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-21 15:22:48 +0200

    Feature: CSV parser complete
    Export csv-parser to be used in main

--- src/csv-parser.js
+++ src/csv-parser.js
@@ -1,19 +1,27 @@
 const fs       = require('fs'),
       readline = require('readline')
-/*
+
 const file_reader = (file_to_parse) => {
   return rl = readline.createInterface({
     input: fs.createReadStream(file_to_parse),
     terminal: false
   })
 }
-*/
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('../testing-files/haulo_credentials_test.csv'),
-    terminal: false
+const pasre_csv_to_array = (file_to_parse) => {
+  const csv = []
+  file_reader(file_to_parse)
+  return new Promise((resolve, reject) => {
+    rl.on('line', input => {
+      csv.push(input.split(','))
+    })
+    rl.on('close', () => {
+      csv.length ? resolve(csv) : reject('Error: empty array')
+    })
   })
+}
 
-rl.on('line', input => {
-  console.log(input)
-})
+
+pasre_csv_to_array('../testing-files/haulo_credentials_test.csv')
+  .then(r => console.log(r))
+  .catch(err => console.log(err))
