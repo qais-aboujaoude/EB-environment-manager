890b1be8f543fcd777ff0524636be887ed466590
Qais Issa Aboujaoude <qiaboujaoude@gmail.com>
2018-02-22 20:39:13 +0200

    Feature - flushEnvVar complete and exportable
    TODO add to the main program in index.js

--- src/flush-env.js
+++ src/flush-env.js
@@ -2,10 +2,19 @@ const eb_sdk  = require('./eb-sdk'),
       exec    = require('child_process').exec
 
 module.exports = {  
+
+  /**
+   * @method flushEnvironmentVariables flushes the environment variables 
+   * of the supplied environment. 
+   * It executes a shell command eb setenv 
+   * on the returned string from @method getEnvironmentVariables
+   * @param {string} app_name name of the elastic beanstalk application
+   * @param {string} env_name name of the environment to flush
+   */
   flushEnvironmentVariables: (app_name, env_name) => {
     eb_sdk.getEnvironmentVariables(app_name, env_name)
     .then(r => {
-      exec(`eb setenv kousa=mehshe`, (err, stdout, stderr) => {
+      exec(`eb setenv ${r}`, (err, stdout, stderr) => {
         err ? console.log(err, stderr) : console.log(stdout)
       })
     })
