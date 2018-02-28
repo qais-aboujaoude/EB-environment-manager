8c73fc9261dde13678fe08b1561eae1f8072a22f
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-02-28 15:25:26 +0200

    Add cd-sdk a wrapper for codebuild-sdk
    Work on new feature to fill the codebuild project with environment
    variables

---
+++ src/cb-sdk.js
@@ -0,0 +1,38 @@
+process.env.AWS_SDK_LOAD_CONFIG = true
+
+const AWS       = require('aws-sdk'),
+      codebuild = new AWS.CodeBuild()
+
+const params = {
+  name: 'custom-build-test',
+  artifacts: {
+    type: 'CODEPIPELINE',
+
+  },
+  cache: {
+    type: 'NO_CACHE'
+  },
+  environment: {
+    computeType: 'BUILD_GENERAL1_SMALL',
+    image: 'sqwirllab/codebuild-node-amazonlinux',
+    type: 'LINUX_CONTAINER',
+    environmentVariables: [
+      {
+        name: 'rattel',
+        value: 'sattel',
+        type: 'PLAINTEXT'
+      },
+    ],
+  },
+  source: {
+    type: 'CODEPIPELINE',
+  }
+}
+
+
+codebuild.updateProject(params, (err, data) => {
+  if (err) console.log(err, err.stack)
+  else     console.log(data)
+})
+
+
